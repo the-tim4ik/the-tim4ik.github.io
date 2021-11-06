ff6744d698e1da801b4483729d54780e71ce3e51
the-tim4ik <85153778+the-tim4ik@users.noreply.github.com>
2021-11-07 00:36:12 +0200

    2.1

--- Clerksy/js/script.js
+++ Clerksy/js/script.js
@@ -1,7 +1,16 @@
 let btnHide = document.querySelector('.menu__btn');
 let div = document.querySelector('.menu__hide');
+let divStyle = document.querySelector('.menu__hide__style');
 btnHide.onclick = function () {
     div.classList.toggle('menu__show');
     btnHide.classList.toggle('menu__btn__move');
     btnHide.classList.toggle('fa-times');
+    divStyle.classList.toggle('menu__hide__style__bg');
 }
+let divClick = document.querySelector('.menu__click');
+// divClick.onclick = function () {
+//     div.classList.remove('menu__show');
+//     btnHide.classList.remove('menu__btn__move');
+//     btnHide.classList.remove('fa-times');
+//     divStyle.classList.remove('menu__hide__style__bg');
+// }
