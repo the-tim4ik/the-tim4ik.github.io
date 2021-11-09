4f7a551da3d41680adfe53815e4603d21813c91e
the-tim4ik <85153778+the-tim4ik@users.noreply.github.com>
2021-11-09 19:43:50 +0200

    1.1

--- bike/js/script.js
+++ bike/js/script.js
@@ -1,15 +1,16 @@
 let btnHide = document.querySelector('.menu__btn');
 let div = document.querySelector('.menu__hide');
 let divStyle = document.querySelector('.menu__hide__style');
+let divClick = document.querySelector('.menu__click');
 btnHide.onclick = function () {
     div.classList.toggle('menu__show');
     btnHide.classList.toggle('menu__btn__move');
-    btnHide.classList.toggle('fa-times');  
+    btnHide.classList.toggle('fa-times');
+    divClick.classList.toggle('menu__click__bg');
 }
-let divClick = document.querySelector('.menu__click');
 divClick.onclick = function () {
     div.classList.remove('menu__show');
     btnHide.classList.remove('menu__btn__move');
     btnHide.classList.remove('fa-times');
-    divStyle.classList.remove('menu__hide__style__bg');
+    divClick.classList.remove('menu__click__bg');
 }
