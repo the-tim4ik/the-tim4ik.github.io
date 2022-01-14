ff817801d377d505cebc79b033e78e506729688a
the-tim4ik <85153778+the-tim4ik@users.noreply.github.com>
2022-01-14 19:52:31 +0200

    1.1

--- Paper/js/script.js
+++ Paper/js/script.js
@@ -5,3 +5,13 @@ btnHide.onclick = function () {
     div.classList.toggle('menu__show');
     body.classList.toggle('body__js');
 }
+
+window.addEventListener('load', function () {
+    let loader = document.querySelector('.preloader');
+    setTimeout(function () {
+        loader.style.opacity = 0;
+    }, 1000)
+    setTimeout(function () {
+        loader.style.display = 'none';
+    }, 1800)
+})
