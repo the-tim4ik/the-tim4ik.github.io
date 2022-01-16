cbf4320b9847f1e171cf133ba4def99310020e80
the-tim4ik <85153778+the-tim4ik@users.noreply.github.com>
2022-01-16 17:54:31 +0200

    3.0

--- Paper/js/script.js
+++ Paper/js/script.js
@@ -1,9 +1,12 @@
+let menu = document.querySelector('.menu');
 let btnHide = document.querySelector('.menu-burger__btn');
 let div = document.querySelector('.menu__hide');
 let body = document.querySelector('.body');
 btnHide.onclick = function () {
     div.classList.toggle('menu__show');
     body.classList.toggle('body__js');
+    btnHide.classList.toggle('fa-times');
+    menu.classList.toggle('fixed');
 }
 
 window.addEventListener('load', function () {
